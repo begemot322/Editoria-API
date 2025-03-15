a39e652f133e18194839261e689e87057c13c471
Николай <kolya.zaitsev6163@gmail.com>
2025-03-15 19:07:22 +0400

    add dependency file in web layer

---
+++ Editoria.Web/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Editoria.Web;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
