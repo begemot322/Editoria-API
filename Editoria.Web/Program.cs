5dbe9214f9c0f0cd64ca495b7c6f3c10e2c8c36f
Николай <kolya.zaitsev6163@gmail.com>
2025-03-15 19:07:46 +0400

    add services

--- Editoria.Web/Program.cs
+++ Editoria.Web/Program.cs
@@ -1,21 +1,21 @@
 using Editoria.Infrastructure;
+using Editoria.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services
+    .AddInfrastructureServices(builder.Configuration)
+    .AddWebServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
