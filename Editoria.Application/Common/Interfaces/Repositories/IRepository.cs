73fc9a02b6e21dded27915dc5052f52eee5f068f
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:41:31 +0400

    add common repository

---
+++ Editoria.Application/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Editoria.Application.Common.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+    Task<T?> GetAsync(Expression<Func<T, bool>>? filter = null);
+    Task<T?> GetByIdAsync(int id);
+    void Delete(T entity);
+    void Update(T entity);
+}
---
+++ Editoria.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using Editoria.Application.Common.Interfaces.Repositories;
+using Editoria.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Editoria.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly DbSet<T> _dbSet; 
+
+    public Repository(ApplicationDbContext db)
+    {
+        _dbSet = db.Set<T>();
+    }
+    
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    
+    public async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        await _dbSet.AddRangeAsync(entities);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+    {
+        IQueryable<T> query = _dbSet;
+        
+        if (filter != null)
+            query = query.Where(filter);
+
+        return await query.AsNoTracking().ToListAsync();
+    }
+    
+    public async Task<T?> GetAsync(Expression<Func<T, bool>>? filter = null)
+    {
+        IQueryable<T> query = _dbSet;
+        
+        if (filter != null)
+            query = query.Where(filter);
+        
+        return await query.AsNoTracking().FirstOrDefaultAsync();
+    }
+    
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+    
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+    
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+}
