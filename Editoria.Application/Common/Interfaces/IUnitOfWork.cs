30dde95eb44e94f7bdf34c416e7e48c4728e6e86
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:46:27 +0400

    add unitofwork

---
+++ Editoria.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Editoria.Application.Common.Interfaces.Repositories;
+
+namespace Editoria.Application.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    IArticleRepository Articles { get; }
+    IAuthorRepository Authors { get; }
+    ICategoryRepository Categories { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ Editoria.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Editoria.Application.Common.Interfaces;
+using Editoria.Application.Common.Interfaces.Repositories;
+using Editoria.Infrastructure.Repositories;
+
+namespace Editoria.Infrastructure.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _db;
+
+    private IArticleRepository _articleRepository;
+    private IAuthorRepository _authorRepository;
+    private ICategoryRepository _categoryRepository;
+
+    public UnitOfWork(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public IArticleRepository Articles => 
+        _articleRepository ??= new ArticleRepository(_db);
+
+    public IAuthorRepository Authors => 
+        _authorRepository ??= new AuthorRepository(_db);
+    
+    public ICategoryRepository Categories => 
+        _categoryRepository ??= new CategoryRepository(_db);
+    
+    public void Dispose()
+    {
+        _db.Dispose();
+    }
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await _db.SaveChangesAsync(cancellationToken);
+    }
+}
