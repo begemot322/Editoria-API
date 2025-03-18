60f0f1a610c4be25619c1be1fd8456b26e068a98
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:40:31 +0400

    add validator for updatecategory

---
+++ Editoria.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Editoria.Application.Features.Categories.Commands.UpdateCategory;
+
+public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+{
+    public UpdateCategoryCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("id не может быть пустым")
+            .GreaterThan(0).WithMessage("id должно быть больше 0");
+        
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Имя не может быть пустым")
+            .NotNull().WithMessage("Имя не может быть null")
+            .MaximumLength(150);
+    }
+}
