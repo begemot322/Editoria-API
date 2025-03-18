56c64e7a9dcca16bda2a5b3290fe6b963a966a43
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:48:49 +0400

    add common application error

---
+++ Editoria.Application/Common/Exceptions/ApplicationException.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace Editoria.Application.Common.Exceptions;
+
+public abstract class ApplicationException(HttpStatusCode statusCode, string message) : Exception(message)
+{
+    public HttpStatusCode ApplicationErrorCode { get; } = statusCode;
+}
