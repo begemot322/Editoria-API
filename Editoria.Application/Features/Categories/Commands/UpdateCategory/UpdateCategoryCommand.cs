09e1160acb43c58bda459f525320ab6aca7297b0
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:50:35 +0400

    add update command

---
+++ Editoria.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
@@ -0,0 +1,19 @@
+﻿using Editoria.Domain.Entities;
+using MediatR;
+
+namespace Editoria.Application.Features.Categories.Commands.UpdateCategory;
+
+
+public class UpdateCategoryResult
+{
+    public Category? Data { get; init; }
+}
+
+public class UpdateCategoryCommand : IRequest<UpdateCategoryResult>
+{
+    public int Id { get; init; }
+    
+    public string Name { get; init; }
+    
+    public string Description { get; init; }
+}
---
+++ Editoria.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Editoria.Application.Common.Exceptions;
+using Editoria.Application.Common.Interfaces;
+using MediatR;
+
+namespace Editoria.Application.Features.Categories.Commands.UpdateCategory;
+
+public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand, UpdateCategoryResult>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateCategoryCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<UpdateCategoryResult> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _unitOfWork.Categories.GetByIdAsync(request.Id);
+
+        if (category == null)
+            throw new NotFoundException(request.Id);
+
+        category.Name = request.Name;
+        category.Description = request.Description;
+
+        _unitOfWork.Categories.Update(category);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new UpdateCategoryResult
+        {
+            Data = category
+        };
+    }
+}
