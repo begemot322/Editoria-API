5fe3097d4ad57736e0e62f4b5b983cfa45f227a8
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:42:41 +0400

    add DependencyInjection in web

--- Editoria.Web/DependencyInjection.cs
+++ Editoria.Web/DependencyInjection.cs
@@ -1,10 +1,13 @@
-﻿namespace Editoria.Web;
+﻿using Editoria.Web.Filters;
+
+namespace Editoria.Web;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddWebServices(this IServiceCollection services)
     {
-        services.AddControllers();
+        services.AddControllers(options=>
+            options.Filters.Add(new ApiExceptionFilter()));
         
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
