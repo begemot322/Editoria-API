4b948ac172f37cfda8ebe9a0cbf2140822376471
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:49:01 +0400

    add DependencyInjection for application

---
+++ Editoria.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Editoria.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Editoria.Application;
+
+public static class DependencyInjection
+{
+    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // MediatR
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
+        
+        // FluentValidation
+        services.AddValidatorsFromAssembly(assembly);
+        
+        
+        return services;
+    }
+    
+}   
