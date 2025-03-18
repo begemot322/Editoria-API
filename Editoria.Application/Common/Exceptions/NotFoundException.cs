402ca964d6d1e4b856b6ed4972a2f7b4c4ed6be6
Николай <kolya.zaitsev6163@gmail.com>
2025-03-18 22:51:04 +0400

    add not found exception

---
+++ Editoria.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using MediatR.NotificationPublishers;
+
+namespace Editoria.Application.Common.Exceptions;
+
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(int id) : base(HttpStatusCode.NotFound, $"Entity with id {id} was not found")
+    {
+        
+    }
+}
