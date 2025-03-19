3c9affd10354490b3ad7b222196c19561817d585
Николай <kolya.zaitsev6163@gmail.com>
2025-03-20 03:48:15 +0400

    add article command(maybe add CategoryIds instead of categoryId)

---
+++ Editoria.Application/Features/Articles/Commands/CreateArticle/CreateArticleCommand.cs
@@ -0,0 +1,19 @@
+﻿using Editoria.Domain.Enums;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Commands.CreateArticle;
+
+public class CreateArticleCommand : IRequest<int>
+{
+    public string Title { get; init; }
+    
+    public string Text { get; init; }
+    
+    public DateTime PublicationDate { get; init; }
+
+    public ArticleStatus Status { get; init; }
+
+    public int AuthorId { get; init; }
+    
+    public int CategoryId { get; init; }
+}
