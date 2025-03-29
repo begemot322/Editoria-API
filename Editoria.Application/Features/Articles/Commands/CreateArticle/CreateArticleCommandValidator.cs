481e0dae5baaa537a8625e773b015b5e2897decb
Николай <kolya.zaitsev6163@gmail.com>
2025-03-29 05:15:01 +0400

    add validator for create article

---
+++ Editoria.Application/Features/Articles/Commands/CreateArticle/CreateArticleCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Editoria.Application.Features.Articles.Commands.CreateArticle;
+
+public class CreateArticleCommandValidator : AbstractValidator<CreateArticleCommand>
+{
+    public CreateArticleCommandValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Заголовок не может быть пустым")
+            .NotNull().WithMessage("Заголовок не может быть null")
+            .MaximumLength(150).WithMessage("Заголовок не может быть длиннее 150 символов");
+        
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Некорректный статус");
+        
+        RuleFor(x => x.AuthorId)
+            .GreaterThan(0).WithMessage("ID автора должен быть положительным");
+        
+        RuleFor(x => x.PublicationDate)
+            .Must(BeNotInFuture).WithMessage("Дата публикации не может быть в будущем");
+    }
+
+    private static bool BeNotInFuture(DateTime dateOfPublish)
+    {
+        return dateOfPublish <= DateTime.Today;
+    }
+}
