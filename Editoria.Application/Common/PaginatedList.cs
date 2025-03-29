e005a57b7f3bd7773fc672fb107b574322ce2826
Николай <kolya.zaitsev6163@gmail.com>
2025-03-29 05:14:06 +0400

    add paginatedList

---
+++ Editoria.Application/Common/PaginatedList.cs
@@ -0,0 +1,22 @@
+﻿namespace Editoria.Application.Common;
+
+public class PaginatedList<T>
+{
+    public IReadOnlyCollection<T> Items { get; }
+    public int PageNumber { get; }
+    public int PageSize { get; }
+    public int TotalPages { get; }
+    public int TotalCount { get; }
+    
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+
+    public PaginatedList(IReadOnlyCollection<T> items, int totalCount, int pageNumber, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        PageNumber = pageNumber;    
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+    }
+}
