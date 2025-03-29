379e2df2b6b04d21fce9004a542777e98e779af3
Николай <kolya.zaitsev6163@gmail.com>
2025-03-29 05:15:19 +0400

    add page query for article

---
+++ Editoria.Application/Features/Articles/Queries/GetPagedArticles/GetPagedArticlesQuery.cs
@@ -0,0 +1,17 @@
+﻿using Editoria.Application.Common;
+using Editoria.Domain.Entities;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Queries.GetPagedArticles;
+
+public class GetPagedArticlesQuery : IRequest<PaginatedList<Article>>
+{
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+
+    public GetPagedArticlesQuery(int pageNumber = 1, int pageSize = 10) 
+    {
+        PageNumber = pageNumber > 0 ? pageNumber : 1; 
+        PageSize = pageSize > 0 ? pageSize : 10;   
+    }
+}
---
+++ Editoria.Application/Features/Articles/Queries/GetPagedArticles/GetPagedArticlesQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Editoria.Application.Common;
+using Editoria.Application.Common.Interfaces;
+using Editoria.Domain.Entities;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Queries.GetPagedArticles;
+
+public class GetPagedArticlesQueryHandler : IRequestHandler<GetPagedArticlesQuery, PaginatedList<Article>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetPagedArticlesQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<PaginatedList<Article>> Handle(GetPagedArticlesQuery request, CancellationToken cancellationToken)
+    {
+        return await _unitOfWork.Articles.GetPagedAsync(request.PageNumber, request.PageSize);
+    }
+}
