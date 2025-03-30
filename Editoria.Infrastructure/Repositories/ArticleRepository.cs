890fa4829ca5257338e0d0f933bcc1edfcfe3fc3
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:29:05 +0400

    add method in repository for then include

--- Editoria.Infrastructure/Repositories/ArticleRepository.cs
+++ Editoria.Infrastructure/Repositories/ArticleRepository.cs
@@ -1,11 +1,27 @@
 ﻿using Editoria.Application.Common.Interfaces.Repositories;
 using Editoria.Domain.Entities;
 using Editoria.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Editoria.Infrastructure.Repositories;
 
 public class ArticleRepository : Repository<Article>, IArticleRepository
 {
-    public ArticleRepository(ApplicationDbContext db) 
-        : base(db) { }
+    private readonly ApplicationDbContext _db;
+
+    public ArticleRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public async Task<Article?> GetArticleWithDetailsAsync(int articleId)
+    {
+        return await _db.Set<Article>()
+            .Include(a => a.Author)
+            .Include(a => a.ArticleCategories)
+                .ThenInclude(ac => ac.Category)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == articleId);
+        
+    }
 }   
