106292b9a15adf23a6343723c2bb7a534f656d8b
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:35:56 +0400

    add query for get article

---
+++ Editoria.Application/Features/Articles/Queries/GetArticleById/GetArticleByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Editoria.Application.Features.Articles.Queries;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Queries.GetArticleById;
+
+public class GetArticleByIdQuery : IRequest<ArticleDto> 
+{
+    public int Id { get; init; }
+    
+    public GetArticleByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
---
+++ Editoria.Application/Features/Articles/Queries/GetArticleById/GetArticleByIdQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using Editoria.Application.Common.Exceptions;
+using Editoria.Application.Common.Interfaces;
+using Editoria.Application.Features.Articles.Queries;
+using Editoria.Domain.Entities;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Queries.GetArticleById;
+
+public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, ArticleDto>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetArticleByIdQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<ArticleDto> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
+    {
+        var article = await _unitOfWork.Articles.GetArticleWithDetailsAsync(request.Id);
+        
+        if (article == null)
+            throw new NotFoundException(request.Id);
+        
+        var articleDto = new ArticleDto
+        {
+            Id = article.Id,
+            Title = article.Title,
+            Text = article.Text,
+            PublicationDate = article.PublicationDate,
+            Status = article.Status.ToString(),
+            Author = new AuthorShortDto()
+            {
+                Id = article.Author.Id,
+                Name = article.Author.Name,
+                Surname = article.Author.Surname,
+            },
+            Categories = article.ArticleCategories
+                .Select(ac => new CategoryShortDto()
+                {
+                    Id = ac.Category.Id,
+                    Name = ac.Category.Name
+                }).ToList()
+        };
+        return articleDto;
+    }
+}
