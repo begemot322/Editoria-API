a1f257e97630d244ee74794a3b6b14678628ce50
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:36:33 +0400

    validator for get category

---
+++ Editoria.Application/Features/Articles/Queries/GetArticleById/GetCategoryByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using Editoria.Application.Features.Categories.Queries.GetCategoryById;
+using FluentValidation;
+
+namespace Editoria.Application.Features.Articles.Queries.GetArticleById;
+
+public class GetCategoryByIdQueryValidator : AbstractValidator<GetCategoryByIdQuery>
+{
+    public GetCategoryByIdQueryValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty()
+            .WithMessage("Id не может быть пустым.");
+    }
+}
