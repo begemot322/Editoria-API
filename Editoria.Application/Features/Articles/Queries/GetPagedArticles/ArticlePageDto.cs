d99fdb26b5e267b2e75ee2b5174b81a32a7c546e
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:28:43 +0400

    add articlepagedto for read pages

---
+++ Editoria.Application/Features/Articles/Queries/GetPagedArticles/ArticlePageDto.cs
@@ -0,0 +1,16 @@
+﻿using Editoria.Domain.Enums;
+
+namespace Editoria.Application.Features.Articles.Queries.GetPagedArticles;
+
+public class ArticlePageDto
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Text { get; set; }
+    
+    public DateTime PublicationDate { get; set; }
+
+    public string Status { get; set; }
+}
