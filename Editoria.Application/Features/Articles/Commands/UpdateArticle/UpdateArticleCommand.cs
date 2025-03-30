a54bcc4aa23aa427a30532c7c180327bdec46ba1
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 23:55:59 +0400

    add update for article

---
+++ Editoria.Application/Features/Articles/Commands/UpdateArticle/UpdateArticleCommand.cs
@@ -0,0 +1,22 @@
+﻿using Editoria.Domain.Entities;
+using Editoria.Domain.Enums;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Commands.UpdateArticle;
+
+public class UpdateArticleCommand : IRequest<Unit>
+{
+    public int Id { get; init; }
+    
+    public string Title { get; init; }
+    
+    public string Text { get; init; }
+    
+    public DateTime PublicationDate { get; init; }
+
+    public ArticleStatus Status { get; init; }
+
+    public int AuthorId { get; init; }
+    
+    public List<int> CategoryIds { get; init; } = new List<int>();
+}
---
+++ Editoria.Application/Features/Articles/Commands/UpdateArticle/UpdateArticleCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Editoria.Application.Common.Exceptions;
+using Editoria.Application.Common.Interfaces;
+using Editoria.Domain.Entities;
+using MediatR;
+
+namespace Editoria.Application.Features.Articles.Commands.UpdateArticle;
+
+public class UpdateArticleCommandHandler : IRequestHandler<UpdateArticleCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateArticleCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(UpdateArticleCommand request, CancellationToken cancellationToken)
+    {
+        var article = await _unitOfWork.Articles.GetArticleWithDetailsAsync(request.Id, tracked:true);
+
+        if (article == null)
+            throw new NotFoundException(request.Id);
+
+        article.Title = request.Title;
+        article.Text = request.Text;
+        article.PublicationDate = request.PublicationDate;
+        article.Status = request.Status;
+        article.AuthorId = request.AuthorId;
+        
+        article.ArticleCategories.Clear(); 
+        
+        var categories = await _unitOfWork.Categories
+            .GetAllAsync(c => request.CategoryIds.Contains(c.Id), tracked:true);
+
+        foreach (var category in categories)    
+        {
+            article.ArticleCategories.Add(new ArticleCategory
+            {
+                Article = article,
+                Category = category
+            });
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
