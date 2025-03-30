4ad8ff73cedccbe1dcd08704502becad537bc1c8
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:32:50 +0400

    change position for result update

--- Editoria.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
+++ Editoria.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
@@ -3,12 +3,6 @@ using MediatR;
 
 namespace Editoria.Application.Features.Categories.Commands.UpdateCategory;
 
-
-public class UpdateCategoryResult
-{
-    public Category? Data { get; init; }
-}
-
 public class UpdateCategoryCommand : IRequest<UpdateCategoryResult>
 {
     public int Id { get; init; }
@@ -16,4 +10,9 @@ public class UpdateCategoryCommand : IRequest<UpdateCategoryResult>
     public string Name { get; init; }
     
     public string Description { get; init; }
+}
+
+public class UpdateCategoryResult
+{
+    public Category? Data { get; set; }
 }
