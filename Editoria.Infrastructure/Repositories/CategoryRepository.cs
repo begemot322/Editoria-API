1fa884967ce30f4e61cd9ffad745417f031760f8
Николай <kolya.zaitsev6163@gmail.com>
2025-03-30 20:30:05 +0400

    add new method in categoryrepository for theninclude

--- Editoria.Infrastructure/Repositories/CategoryRepository.cs
+++ Editoria.Infrastructure/Repositories/CategoryRepository.cs
@@ -1,11 +1,25 @@
 ﻿using Editoria.Application.Common.Interfaces.Repositories;
 using Editoria.Domain.Entities;
 using Editoria.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Editoria.Infrastructure.Repositories;
 
 public class CategoryRepository : Repository<Category>, ICategoryRepository
 {
-    public CategoryRepository(ApplicationDbContext db) 
-        : base(db) { }
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+    
+    public async Task<Category?> GetCategoryWithArticlesAsync(int categoryId)
+    {
+        return await _db.Set<Category>()
+            .Include(a => a.ArticleCategories)
+                .ThenInclude(ac => ac.Article)
+            .FirstOrDefaultAsync(a => a.Id == categoryId);
+        
+    }
 }
