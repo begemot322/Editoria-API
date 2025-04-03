4f9e083f440a7c5d296f64e3ac3fb9820f99c908
Николай <kolya.zaitsev6163@gmail.com>
2025-04-04 03:24:50 +0400

    add new dto

---
+++ Editoria.Application/Features/Articles/Queries/Dtos/ArticlePageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Editoria.Application.Features.Articles.Queries.Dtos;
+
+public class ArticlePageDto
+{
+    public int Id { get; init; }
+    public string Title { get; init; } 
+    public string Text { get; init; } 
+    public DateTime PublicationDate { get; init; }
+    public string Status { get; init; } 
+
+}
