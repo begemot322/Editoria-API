2cdce45607a6ad6d216e243605395630c10d912d
Николай <kolya.zaitsev6163@gmail.com>
2025-04-05 21:12:36 +0400

    separate services.

--- Editoria.Infrastructure/DependencyInjection.cs
+++ Editoria.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Editoria.Application.Common.Interfaces;
+using Editoria.Infrastructure.Cache;
 using Editoria.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -11,12 +12,35 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddDatabase(configuration);
+        services.AddRedisCache(configuration);
+
+        return services;
+    }
+    private static IServiceCollection AddDatabase(this IServiceCollection services,
+        IConfiguration configuration)
+    {
         var connString = configuration.GetConnectionString("DefaultConnection");
-        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connString));
+        services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseNpgsql(connString));
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         return services;
     }
+    private static IServiceCollection AddRedisCache(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration.GetConnectionString("Redis");
+            options.InstanceName = "Editoria_";
+        });
+        
+        services.AddScoped<IRedisCacheService, RedisCacheService>();
+        
+        return services;
+    }
+
 
 }
