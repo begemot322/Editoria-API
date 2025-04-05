50c0906e280c2a88eaa71584e0d6d36dc6318467
Николай <kolya.zaitsev6163@gmail.com>
2025-04-05 23:13:31 +0400

    add authors query

---
+++ Editoria.Application/Features/Authors/Queries/GetAuthors/GetAuthorsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Editoria.Application.Features.Authors.Queries.Dtos;
+using MediatR;
+
+namespace Editoria.Application.Features.Authors.Queries.GetAuthors;
+
+public class GetAuthorsQuery : IRequest<IEnumerable<AuthorListDto>>
+{
+    
+}
---
+++ Editoria.Application/Features/Authors/Queries/GetAuthors/GetAuthorsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Editoria.Application.Common.Interfaces;
+using Editoria.Application.Features.Authors.Queries.Dtos;
+using MediatR;
+
+namespace Editoria.Application.Features.Authors.Queries.GetAuthors;
+
+public class GetAuthorsQueryHandler : IRequestHandler<GetAuthorsQuery, IEnumerable<AuthorListDto>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAuthorsQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<IEnumerable<AuthorListDto>> Handle(GetAuthorsQuery request, CancellationToken cancellationToken)
+    {
+        var authors = await _unitOfWork.Authors.GetAllAsync();
+
+        return authors.Select(author => new AuthorListDto
+        {
+            Id = author.Id,
+            Name = author.Name,
+            Surname = author.Surname,
+            PhoneNumber = author.PhoneNumber,
+            Biography = author.Biography
+        });
+    }
+}
