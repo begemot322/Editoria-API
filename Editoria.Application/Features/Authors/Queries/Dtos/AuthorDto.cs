57fd427e32865de5303cbbd6a98a7e057f365bce
Николай <kolya.zaitsev6163@gmail.com>
2025-04-05 23:12:36 +0400

    add author dtos

---
+++ Editoria.Application/Features/Authors/Queries/Dtos/AuthorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Editoria.Application.Features.Authors.Queries.Dtos;
+
+public class AuthorDto
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Surname { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Biography { get; set; }
+    
+    public List<int> ArtilesIds { get; init; } = new();
+}
---
+++ Editoria.Application/Features/Authors/Queries/Dtos/AuthorListDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Editoria.Application.Features.Authors.Queries.Dtos;
+
+public class AuthorListDto
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Surname { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Biography { get; set; }
+
+}
